09b6026b7c339d7bf4b91f1ddb4766495d418485
Chad Faurie <chad@codeworkshop.io>
2020-04-08 08:16:30 +0200

    additional config parameters

--- packages/cubejs-elasticsearch-driver/driver/ElasticSearchDriver.js
+++ packages/cubejs-elasticsearch-driver/driver/ElasticSearchDriver.js
@@ -2,20 +2,31 @@ const { Client } = require('@elastic/elasticsearch');
 const SqlString = require('sqlstring');
 const BaseDriver = require('@cubejs-backend/query-orchestrator/driver/BaseDriver');
 
+const dialects = {
+  awselasticsearch: 'awselasticsearch',
+  odelasticsearch: 'odelasticsearch',
+  elasticsearch: 'elasticsearch',
+};
+
 class ElasticSearchDriver extends BaseDriver {
   constructor(config) {
     super();
+
+    console.log(config);
+
+    const dialect = dialects[process.env.CUBEJS_DB_TYPE];
 
     // TODO: This config applies to AWS ES, Native ES and OpenDistro ES
     // All 3 have different dialects according to their respective documentation
     this.config = {
       url: process.env.CUBEJS_DB_URL,
-      openDistro:
-        (process.env.CUBEJS_DB_ELASTIC_OPENDISTRO || 'false').toLowerCase() === 'true' ||
-        process.env.CUBEJS_DB_TYPE === 'odelasticsearch',
+      // openDistro:
+      //   (process.env.CUBEJS_DB_ELASTIC_OPENDISTRO || 'false').toLowerCase() === 'true' ||
+      //   process.env.CUBEJS_DB_TYPE === 'odelasticsearch',
+      dialect,
       ...config
     };
-    this.client = new Client({ node: this.config.url, cloud: this.config.cloud });
+    this.client = new Client({ node: this.config.url, cloud: this.config.cloud, auth: this.config.auth, ...config });
     this.sqlClient = this.config.openDistro ? new Client({ node: `${this.config.url}/_opendistro` }) : this.client;
   }
 
@@ -33,16 +44,22 @@ class ElasticSearchDriver extends BaseDriver {
         }
       })).body;
 
-      // TODO: Clean this up, will need a better identifier than the cloud setting
-      if (this.config.cloud) {
-        const compiled = result.rows.map(
-          r => result.columns.reduce((prev, cur, idx) => ({ ...prev, [cur.name]: r[idx] }), {})
-        );
-
-        return compiled;
+      if (result.error) {
+        throw Error(JSON.stringify(result.error, null, 2));
       }
 
-      return result && result.aggregations && this.traverseAggregations(result.aggregations);
+      switch (this.config.dialect) {
+        // Elastic Search - X PACK
+        case dialects.elasticsearch:
+          return this.traverseXPackResult(result);
+        // AWS ElasticSearch
+        // Open Distro
+        case dialects.awselasticsearch:
+        case dialects.odelasticsearch:
+          return this.traverseODResult(result);
+        default:
+          throw Error("Not Implemented");
+      }
     } catch (e) {
       if (e.body) {
         throw new Error(JSON.stringify(e.body, null, 2));
@@ -50,6 +67,22 @@ class ElasticSearchDriver extends BaseDriver {
 
       throw e;
     }
+  }
+
+  traverseXPackResult(result) {
+    return result.rows.map(
+      r => result.columns.reduce((prev, cur, idx) => ({ ...prev, [cur.name]: r[idx] }), {})
+    );
+  }
+
+  traverseODResult(result) {
+    const records = result.datarows.map(
+      r => result.schema.reduce((prev, cur, idx) => ({ ...prev, [cur.alias]: r[idx] }), {})
+    );
+
+    return records;
+
+    // return result && result.aggregations && this.traverseAggregations(result.aggregations);
   }
 
   traverseAggregations(aggregations) {
